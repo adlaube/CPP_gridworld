ab99ccd4d539e434b3b3ad4ef1ebe64e076247e2
Adrian Lauber <lauber.adrian@gmail.com>
2020-08-09 22:14:07 +0200

    integrate optimization goal

--- src/mdpsolve/Policies/greedy.cpp
+++ src/mdpsolve/Policies/greedy.cpp
@@ -10,25 +10,31 @@ static GreedyConstructor greedy_constructor;
 void Greedy::updatePolicy(const Model& mdp, const Evaluation& eval) {
 
     uint16_t num_of_states = mdp.num_of_states;
-    double reward, state_probability,total_reward, highest_reward;
+    double reward, state_probability,total_reward, best_reward_value;
 
     for (STATE_ID current_state = 0;current_state<num_of_states;current_state++){
-        for(ACTION_ID action_idx = 0,best_action_idx=0;action_idx<mdp.num_of_actions;action_idx++){
+        for(ACTION_ID   action_idx = 0,best_action_idx=0,best_reward_value=0;
+                        action_idx<mdp.num_of_actions;
+                        action_idx++){
             total_reward = 0;
             for(STATE_ID state_idx = 0;state_idx<num_of_states;state_idx++){
 
                 state_probability = mdp.state_transition_matrix(action_idx,current_state,state_idx);
                 reward = mdp.reward_matrix(action_idx,current_state,state_idx);
-                total_reward = total_reward + state_probability * (reward + eval.getValueOfState(state_idx));
-            }
-            //init highest reward with reward of first action
-            if(action_idx==0){
-                highest_reward = total_reward;
+                total_reward =  total_reward 
+                                + state_probability * 
+                                (reward + eval.getValueOfState(state_idx));
             }
 
-            if(total_reward > highest_reward){
+            if(mdp.optGoal == OPT_MAXIMIZE && total_reward > best_reward_value){
                 best_action_idx = action_idx;
-            }   
+                best_reward_value = total_reward;
+            }
+
+            if(mdp.optGoal == OPT_MINIMIZE && total_reward < best_reward_value){
+                best_action_idx = action_idx;
+                best_reward_value = total_reward;                
+            }
             //set best action as new action
             policy_mapping_(0,0,current_state) = best_action_idx;                     
         }
