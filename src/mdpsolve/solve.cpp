a3b3838107b34ae9eb3f593f1e687f71d771f952
Adrian Lauber <lauber.adrian@gmail.com>
2020-08-13 12:11:20 +0200

    doc, now formatting

--- src/mdpsolve/solve.cpp
+++ src/mdpsolve/solve.cpp
@@ -3,30 +3,32 @@
 using PARSER_FACTORY = Factory<Parser>;
 using POLICY_FACTORY = Factory<Policy>;
 using EVALUATION_FACTORY = Factory<Evaluation>;
-using SOLVER_FACTORY = Factory<Solver>; 
+using SOLVER_FACTORY = Factory<Solver>;
 
-std::unique_ptr<Policy> MdpSolve::solve_filebased_mdp(const MdpSolve::Params& params){
+std::unique_ptr<Policy> MdpSolve::solve_filebased_mdp(const MdpSolve::Params &params)
+{
 
-    Model newmdp;
-    auto parser = PARSER_FACTORY::getInstance().createInstance(params.module_parser_,newmdp);
-    parser->parseFile(params.mdp_filepath_);
+        Model newmdp;
+        auto parser = PARSER_FACTORY::getInstance().createInstance(params.module_parser_, newmdp);
+        parser->parseFile(params.mdp_filepath_);
 
-    auto policy = POLICY_FACTORY::getInstance().createInstance(params.module_policy_,newmdp);
-    auto eval = EVALUATION_FACTORY::getInstance().createInstance(params.module_eval_,newmdp);    
-    auto solver = SOLVER_FACTORY::getInstance().createInstance(params.module_solver_,newmdp);        
+        auto policy = POLICY_FACTORY::getInstance().createInstance(params.module_policy_, newmdp);
+        auto eval = EVALUATION_FACTORY::getInstance().createInstance(params.module_eval_, newmdp);
+        auto solver = SOLVER_FACTORY::getInstance().createInstance(params.module_solver_, newmdp);
 
-    solver->solve(*eval,*policy,params.solver_iteration_cnt_);
+        solver->solve(*eval, *policy, params.solver_iteration_cnt_);
 
-    return policy;
+        return policy;
 }
 
-std::unique_ptr<Policy> MdpSolve::solve_external_mdp(Model& model,const MdpSolve::Params& params){
+std::unique_ptr<Policy> MdpSolve::solve_external_mdp(Model &model, const MdpSolve::Params &params)
+{
 
-    auto policy = POLICY_FACTORY::getInstance().createInstance(params.module_policy_,model);
-    auto eval = EVALUATION_FACTORY::getInstance().createInstance(params.module_eval_,model);    
-    auto solver = SOLVER_FACTORY::getInstance().createInstance(params.module_solver_,model);        
+        auto policy = POLICY_FACTORY::getInstance().createInstance(params.module_policy_, model);
+        auto eval = EVALUATION_FACTORY::getInstance().createInstance(params.module_eval_, model);
+        auto solver = SOLVER_FACTORY::getInstance().createInstance(params.module_solver_, model);
 
-    solver->solve(*eval,*policy,params.solver_iteration_cnt_);
+        solver->solve(*eval, *policy, params.solver_iteration_cnt_);
 
-    return policy;
+        return policy;
 }
